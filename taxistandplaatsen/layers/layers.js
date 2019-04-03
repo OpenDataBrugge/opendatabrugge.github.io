30335e3f646fb641587a01fad03b3fcde4c1fbd2
OpenDataBrugge <steven.devriendt@brugge.be>
2019-04-03 14:27:53 +0200

    change

--- taxistandplaatsen/layers/layers.js
+++ taxistandplaatsen/layers/layers.js
@@ -1,36 +1,50 @@
 var wms_layers = [];
-var format_Taxistandplaatsen_0 = new ol.format.GeoJSON();
-var features_Taxistandplaatsen_0 = format_Taxistandplaatsen_0.readFeatures(json_Taxistandplaatsen_0, 
+var lyr_GRBbasiskaartgrijswaarden_0 = new ol.layer.Tile({
+                            source: new ol.source.TileWMS(({
+                              url: "http://geoservices.informatievlaanderen.be/raadpleegdiensten/GRB-basiskaart-grijs/wms",
+    attributions: '<a href=""></a>',
+                              params: {
+                                "LAYERS": "GRB_BSK_GRIJS",
+                                "TILED": "true",
+                                "VERSION": "1.3.0"},
+                            })),
+                            title: "GRB-basiskaart-grijswaarden",
+                            opacity: 0.532000,
+                            
+                            
+                          });
+              wms_layers.push([lyr_GRBbasiskaartgrijswaarden_0, 0]);var format_Taxistandplaatsen_1 = new ol.format.GeoJSON();
+var features_Taxistandplaatsen_1 = format_Taxistandplaatsen_1.readFeatures(json_Taxistandplaatsen_1, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_Taxistandplaatsen_0 = new ol.source.Vector({
+var jsonSource_Taxistandplaatsen_1 = new ol.source.Vector({
     attributions: '<a href=""></a>',
 });
-jsonSource_Taxistandplaatsen_0.addFeatures(features_Taxistandplaatsen_0);var lyr_Taxistandplaatsen_0 = new ol.layer.Vector({
+jsonSource_Taxistandplaatsen_1.addFeatures(features_Taxistandplaatsen_1);var lyr_Taxistandplaatsen_1 = new ol.layer.Vector({
                 declutter: true,
-                source:jsonSource_Taxistandplaatsen_0, 
-                style: style_Taxistandplaatsen_0,
-                title: '<img src="styles/legend/Taxistandplaatsen_0.png" /> Taxistandplaatsen'
-            });var format_Taxistandplaatsen200m_1 = new ol.format.GeoJSON();
-var features_Taxistandplaatsen200m_1 = format_Taxistandplaatsen200m_1.readFeatures(json_Taxistandplaatsen200m_1, 
+                source:jsonSource_Taxistandplaatsen_1, 
+                style: style_Taxistandplaatsen_1,
+                title: '<img src="styles/legend/Taxistandplaatsen_1.png" /> Taxistandplaatsen'
+            });var format_Taxistandplaatsen200m_2 = new ol.format.GeoJSON();
+var features_Taxistandplaatsen200m_2 = format_Taxistandplaatsen200m_2.readFeatures(json_Taxistandplaatsen200m_2, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_Taxistandplaatsen200m_1 = new ol.source.Vector({
+var jsonSource_Taxistandplaatsen200m_2 = new ol.source.Vector({
     attributions: '<a href=""></a>',
 });
-jsonSource_Taxistandplaatsen200m_1.addFeatures(features_Taxistandplaatsen200m_1);var lyr_Taxistandplaatsen200m_1 = new ol.layer.Vector({
+jsonSource_Taxistandplaatsen200m_2.addFeatures(features_Taxistandplaatsen200m_2);var lyr_Taxistandplaatsen200m_2 = new ol.layer.Vector({
                 declutter: true,
-                source:jsonSource_Taxistandplaatsen200m_1, 
-                style: style_Taxistandplaatsen200m_1,
-                title: '<img src="styles/legend/Taxistandplaatsen200m_1.png" /> Taxistandplaatsen200m'
+                source:jsonSource_Taxistandplaatsen200m_2, 
+                style: style_Taxistandplaatsen200m_2,
+                title: '<img src="styles/legend/Taxistandplaatsen200m_2.png" /> Taxistandplaatsen200m'
             });
 
-lyr_Taxistandplaatsen_0.setVisible(true);lyr_Taxistandplaatsen200m_1.setVisible(true);
-var layersList = [lyr_Taxistandplaatsen_0,lyr_Taxistandplaatsen200m_1];
-lyr_Taxistandplaatsen_0.set('fieldAliases', {'id': 'id', 'Naam': 'Naam', });
-lyr_Taxistandplaatsen200m_1.set('fieldAliases', {'id': 'id', 'Naam': 'Naam', });
-lyr_Taxistandplaatsen_0.set('fieldImages', {'id': 'TextEdit', 'Naam': 'TextEdit', });
-lyr_Taxistandplaatsen200m_1.set('fieldImages', {'id': 'TextEdit', 'Naam': 'TextEdit', });
-lyr_Taxistandplaatsen_0.set('fieldLabels', {'id': 'no label', 'Naam': 'no label', });
-lyr_Taxistandplaatsen200m_1.set('fieldLabels', {'id': 'no label', 'Naam': 'no label', });
-lyr_Taxistandplaatsen200m_1.on('precompose', function(evt) {
+lyr_GRBbasiskaartgrijswaarden_0.setVisible(true);lyr_Taxistandplaatsen_1.setVisible(true);lyr_Taxistandplaatsen200m_2.setVisible(true);
+var layersList = [lyr_GRBbasiskaartgrijswaarden_0,lyr_Taxistandplaatsen_1,lyr_Taxistandplaatsen200m_2];
+lyr_Taxistandplaatsen_1.set('fieldAliases', {'id': 'id', 'Naam': 'Naam', });
+lyr_Taxistandplaatsen200m_2.set('fieldAliases', {'id': 'id', 'Naam': 'Naam', });
+lyr_Taxistandplaatsen_1.set('fieldImages', {'id': 'TextEdit', 'Naam': 'TextEdit', });
+lyr_Taxistandplaatsen200m_2.set('fieldImages', {'id': 'TextEdit', 'Naam': 'TextEdit', });
+lyr_Taxistandplaatsen_1.set('fieldLabels', {'id': 'no label', 'Naam': 'no label', });
+lyr_Taxistandplaatsen200m_2.set('fieldLabels', {'id': 'no label', 'Naam': 'no label', });
+lyr_Taxistandplaatsen200m_2.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
---
+++ taxistandplaatsen/styles/Taxistandplaatsen_1_style.js
@@ -0,0 +1,33 @@
+var size = 0;
+var placement = 'point';
+
+var style_Taxistandplaatsen_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    size = 0;
+    var labelFont = "10px, sans-serif";
+    var labelFill = "rgba(0, 0, 0, 1)";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    var style = [ new ol.style.Style({
+        text: new ol.style.Text({
+            text: 'I',
+            fill: new ol.style.Fill({color: 'rgba(227,26,28,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
