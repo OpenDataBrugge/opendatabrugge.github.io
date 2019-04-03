521e06e70f49f5ebb57b0cbcd1c8785f94795b3f
OpenDataBrugge <steven.devriendt@brugge.be>
2019-04-03 16:46:19 +0200

    test
    test

---
+++ taxistandplaatsen/data/Taxistandplaatsen_3.js
@@ -0,0 +1,15 @@
+var json_Taxistandplaatsen_3 = {
+"type": "FeatureCollection",
+"name": "Taxistandplaatsen_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Naam": "Markt" }, "geometry": { "type": "Point", "coordinates": [ 3.224918849369879, 51.208687314815286 ] } },
+{ "type": "Feature", "properties": { "id": "3", "Naam": "Bargeplein" }, "geometry": { "type": "Point", "coordinates": [ 3.225766401059953, 51.197410017870922 ] } },
+{ "type": "Feature", "properties": { "id": "4", "Naam": "Station (voorkant)" }, "geometry": { "type": "Point", "coordinates": [ 3.218548453064215, 51.198105220720429 ] } },
+{ "type": "Feature", "properties": { "id": "4", "Naam": "Station (achterkant)" }, "geometry": { "type": "Point", "coordinates": [ 3.216133327649708, 51.195639537698547 ] } },
+{ "type": "Feature", "properties": { "id": "5", "Naam": "Baron de Maerelaan" }, "geometry": { "type": "Point", "coordinates": [ 3.183763979839136, 51.329349187454021 ] } },
+{ "type": "Feature", "properties": { "id": "2", "Naam": "Boeveriestraat" }, "geometry": { "type": "Point", "coordinates": [ 3.217070323398625, 51.203920392329763 ] } },
+{ "type": "Feature", "properties": { "id": "6", "Naam": "Vlamingstraat" }, "geometry": { "type": "Point", "coordinates": [ 3.224403509320847, 51.21072439286705 ] } },
+{ "type": "Feature", "properties": { "id": "7", "Naam": "Kuipersstraat" }, "geometry": { "type": "Point", "coordinates": [ 3.223161272735907, 51.209801357744972 ] } }
+]
+}
